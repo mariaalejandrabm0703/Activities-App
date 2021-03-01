2029f7a0df2a5a485b28694c43f0d2c50dae62b4
Maria <mariaalejandrabm0703@gmail.com>
2021-03-01 09:42:26 -0500

    Se adiciona la logica para mover las actividades entre fases.

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,31 +1,73 @@
 import React from "react";
 
-export const Control = ({ currentAct,activities, setActivities }) => {
-
-
-    const handleLeft = (e) =>{
-        e.preventDefault();
-        let list = []
-        let pos = 0
-        console.log(currentAct)
-        for(let i=0; i < activities.length; i++){
-            for(let j=0; j < activities[i].length; j++){                
-                if(currentAct === activities[i][j]){
-                    console.log(i,j, activities[i][j])
-                }
-            }
-        }
+const searchPosition = (list, currentAct) =>{
+  let position = 0;
+  // busco posicion del elemento a mover
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].includes(currentAct)) {
+      position = i;
     }
+  }
+  return position;
+}
 
-    const handleRight = (e) =>{
-        e.preventDefault();   
-     }
+const deleteItem = (list, currentAct) =>{
+  // elimino el elemento a mover
+  return list.map((e) => e.filter((m) => m !== currentAct));
+}
+
+export const Control = ({ currentAct, activities, setActivities }) => {
+  const handleLeft = (e) => {
+    e.preventDefault();
+    let list = activities.slice();
+    let position = 0;
+
+    // busco posicion del elemento a mover
+    position = searchPosition(list, currentAct)
+
+    // elimino el elemento a mover
+    list = deleteItem(list, currentAct);
+
+    // se adiciona el elemento en la nueva posición
+    position = position - 1;
+    position < 0
+      ? (list[list.length - 1][list[list.length - 1].length] = currentAct)
+      : (list[position][list[position].length] = currentAct);
+
+    // se modifica el estado
+    setActivities((activities) => list);
+  };
+
+  const handleRight = (e) => {
+    e.preventDefault();
+    let list = activities.slice();
+    let position = 0;
+
+    // busco posicion del elemento a mover
+    position = searchPosition(list, currentAct)
+
+    // elimino el elemento a mover
+    list = deleteItem(list, currentAct);
+
+    // se adiciona el elemento en la nueva posición
+    position = position + 1;
+    position > (list.length - 1)
+      ? (list[0][list[0].length] = currentAct)
+      : (list[position][list[position].length] = currentAct);
+
+    // se modifica el estado
+    setActivities((activities) => list);
+  };
 
   return (
     <div className="container mt-2">
       <input defaultValue={currentAct} type="text" />
-      <button onClick={handleLeft}  className="btn btn-primary ms-3">{"<"}</button>
-      <button onClick={handleRight} className="btn btn-primary ms-1">{">"}</button>
+      <button onClick={handleLeft} className="btn btn-primary ms-3">
+        {"<"}
+      </button>
+      <button onClick={handleRight} className="btn btn-primary ms-1">
+        {">"}
+      </button>
     </div>
   );
 };
